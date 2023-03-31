4e2fe1f7aea2c1db0afb46c69ff05b0ac9f8db3c
Ryvqi <rifqikhabibi03@gmail.com>
2023-03-31 10:59:58 +0700

    membuat input

--- Quicksort/Quicksort.cpp
+++ Quicksort/Quicksort.cpp
@@ -5,7 +5,19 @@ using namespace std;
 int arr[20];
 int cmp_count = 0; // number of comparasion
 int mov_count = 0; // number of movement
-int n; 
+int n;
+void input() {
+	while (true)
+	{
+		cout << "Masukan panjang elemen array :";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\n------------------------" << endl;
+	}
+}
 int main()
 {
 
