312844c39debc7e47783875f03e99e5bcf291b5c
Ryvqi <rifqikhabibi03@gmail.com>
2023-03-31 10:55:19 +0700

    membuat n

--- Quicksort/Quicksort.cpp
+++ Quicksort/Quicksort.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
+
+//array of integrers to hold values
+int arr[20];
+int cmp_count = 0; // number of comparasion
+int mov_count = 0; // number of movement
+int n; 
 int main()
 {
 
